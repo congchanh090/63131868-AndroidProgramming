2c6c814b5a13258441781220a88154827bbd8275
Lê Công Chánh <congchanh2003@gmail.com>
2025-03-10 10:33:20 +0700

    Done Ex7

---
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ex7_intentlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnChuyenTrang = findViewById(R.id.btnLogin);
+
+        btnChuyenTrang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this, ActivityLogin.class);
+
+                startActivity(iLogin);
+            }
+        });
+    }
+}
