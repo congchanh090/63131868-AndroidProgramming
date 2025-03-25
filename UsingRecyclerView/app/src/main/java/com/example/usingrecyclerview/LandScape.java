b8df21438001aec7021bc27459d16d548bf90756
Lê Công Chánh <congchanh2003@gmail.com>
2025-03-25 23:48:06 +0700

    update

---
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScape.java
@@ -0,0 +1,28 @@
+package com.example.usingrecyclerview;
+
+public class LandScape {
+
+    private String landscapeName;
+    private String landscapeImage;
+
+    public LandScape(String landscapeName, String landscapeImage) {
+        this.landscapeName = landscapeName;
+        this.landscapeImage = landscapeImage;
+    }
+
+    public String getLandscapeName() {
+        return landscapeName;
+    }
+
+    public void setLandscapeName(String landscapeName) {
+        this.landscapeName = landscapeName;
+    }
+
+    public String getLandscapeImage() {
+        return landscapeImage;
+    }
+
+    public void setLandscapeImage(String landscapeImage) {
+        this.landscapeImage = landscapeImage;
+    }
+}
